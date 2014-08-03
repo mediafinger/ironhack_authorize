447e0321f32a133c57618066a10cf1c75d8066cd
Andreas Finger <andreas.finger@xing.com>
2014-08-03 15:37:57 +0200

    Seed the database with Users, Projects, Tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Example:
+
+if Rails.env == "development"
+  puts "Cleaning DB first ..."
+  User.destroy_all
+  Project.destroy_all
+  Task.destroy_all
+end
+
+PROJECTS = 10
+
+puts "Creating Users ..."
+User.create!(email: "ana@example.com")
+User.create!(email: "bella@example.com")
+User.create!(email: "clara@example.com")
+User.create!(email: "dora@example.com")
+
+puts "Creating Projects ..."
+(1..PROJECTS).each do |index|
+  Project.create!(name: "Project #{index}")
+end
+
+projects = Project.all.to_a
+User.all.to_a.each do |user, index|
+  (1..3).each do
+    user.projects << projects.sample
+  end
+
+  puts "Creating Tasks ..."
+  user.projects.each do |project|
+    (1..rand(2..4)).each do |i|
+      project.tasks.create!(name: "Task #{i}", status: %w(todo doing done).sample, user: user)
+    end
+  end
+end
