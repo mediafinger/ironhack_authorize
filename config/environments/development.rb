ff5aba0d869aba7c7d5cad8ed2c615f30d57d680
Andreas Finger <andreas.finger@xing.com>
2014-08-03 22:53:26 +0200

    Prepare App to send mails and to display them...
    ... in the browser in development, through the letter_opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Set base url of App
+  config.base_url = "http://localhost:3000/"
+
+  # Care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  # Send mail (yes, really!)
+  config.action_mailer.perform_deliveries = true
+  # Set delivery method for development to :letter_opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
