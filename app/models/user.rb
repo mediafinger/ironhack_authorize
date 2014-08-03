5c0994717218426817c19c9b54ea2313a8cc8f08
Andreas Finger <andreas.finger@xing.com>
2014-08-03 18:22:56 +0200

    Use has_secure_password to create save passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,18 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :projects
   has_many                :tasks
 
-  attr_accessor :password
+  # has_secure_password is a Rails helper
+  # it comes with an attr_accessor for :password, that's why we removed it
+  # it also triggers some validations on password, as we wrote the validations already,
+  # we pass the parameter false to the helper
+  has_secure_password validations: false
+
+  # has_secure_password uses Bcrypt to generate hashed and salted passwords
+  # doing this manually could look like this:
+  # require 'digest/sha1'
+  # salt = Digest::SHA1.hexdigest("Add #{email} as unique value and #{Time.now} as random value")
+  # encrypted_password = Digest::SHA1.hexdigest("Adding #{salt} to #{password}")
+  # you would need to save the salt and the encrypted_password in the database
 
   # very simple email matcher ~ "includes an @ and a . and some charachters around them"
   # real email validation will happen over activation email
