0847420b7a2cbe140199a4eeb3f8fe7708a38ede
Andreas Finger <andreas.finger@xing.com>
2014-08-03 15:35:55 +0200

    Create Relation between Tasks and Users

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+  belongs_to :user
 
-  validates :name,   presence: true
-  validates :status, inclusion: { in: %w(todo doing done), message: "%{value} is not a valid status" }
+  validates :name,    presence: true
+  validates :project, presence: true
+  validates :user,    presence: true
+  validates :status,  inclusion: { in: %w(todo doing done), message: "%{value} is not a valid status" }
 
   scope :todo,  -> { where(status: "todo")  }
   scope :doing, -> { where(status: "doing") }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :projects
+  has_many                :tasks
 
   # Regular Expression to validate emails:
   # regex = []
