22165e95030a3b296c74f1ed47017d7a34426475
Andreas Finger <andy@mediafinger.com>
2014-08-10 15:09:48 +0200

    Display nicer error message when an action is forbidden

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -19,4 +21,14 @@ class ApplicationController < ActionController::Base
     end
   end
   helper_method :current_user   # to be able to use it from the views
+
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+
+    flash[:error] = "You cannot perform the action #{exception.query} according to the #{policy_name}."
+    redirect_to(request.referrer || root_path)
+  end
 end
