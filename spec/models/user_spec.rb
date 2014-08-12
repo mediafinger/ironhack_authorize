6cbc80adf293932db8832101250dae9b760b4687
Andreas Finger <andy@mediafinger.com>
2014-08-12 10:29:51 +0200

    Add more user specs to demonstrate one-liners

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,54 @@ require 'rails_helper'
 
 describe User do
 
+  describe "when ensuring that the user model is valid" do
+    # User objects usually have many fields... so better create a user factory!
+    before do
+      @user = User.new(email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    end
+
+    # the one liners will all test the subject
+    subject { @user }
+
+    # one-liner syntax - here the "should" can still be used with Rspec 3
+    # the first example is equal to:
+    # it { is_expected.to respond_to(:email) }
+    # decide yourself if you like more consistency (using expect) or the shorter should
+
+    it { should respond_to(:email) }
+    it { should respond_to(:password_digest) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+
+    it { should be_valid }
+  end
+
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(email: "user@example.com", password: " ", password_confirmation: " ")
+    end
+
+    # same test in one-liner syntax
+    it { should_not be_valid }
+
+    # same test in more verbose block syntax
+    it "should not be valid" do
+      expect(@user).to_not be_valid
+    end
+  end
+
+
+  describe "when password doesn't match confirmation" do
+    # using let instead of before block - just for demonstration, you should be consistent ;-)
+    let(:user) { User.new(email: "user@example.com", password: "123456", password_confirmation: " ") }
+
+    before { user.password_confirmation = "mismatch" }
+
+    it { should_not be_valid }
+  end
+
+
   describe "Email Validations" do
     context "on a new user" do
       it "should not be valid without a email" do
