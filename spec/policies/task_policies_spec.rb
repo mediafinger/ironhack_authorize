6082cbfbc486203f3d63ef715a152c96dd150256
Andreas Finger <andy@mediafinger.com>
2014-08-13 21:38:42 +0200

    Add the missing TaskPolicy specs

--- spec/policies/task_policies_spec.rb
+++ spec/policies/task_policies_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe TaskPolicy do
+  # all the tests run on this subject - in every context block we define "user" and "task" a bit different
   subject { TaskPolicy.new(user, task) }
 
   context "for a developer" do
@@ -29,6 +30,21 @@ describe TaskPolicy do
     it { should_not permit(:destroy) }
   end
 
+  context "for a po on tasks of another user" do
+    let(:user)    { User.create!(email: "test@example.com", password: "test", confirmed: true, roles: ["po"]) }
+    let(:dev)     { User.create!(email: "dev@example.com", password: "test", confirmed: true, roles: ["developer"]) }
+    let(:project) { Project.create!(name: "Project 1") }
+    let(:task)    { dev.tasks.create(name: "Task 2", status: "todo", project: project) }
+
+    it { should     permit(:show)    }
+    it { should_not permit(:create)  }  # fix the policy
+    it { should_not permit(:new)     }  # fix the policy
+    it { should_not permit(:update)  }  # write the test in another way
+    it { should_not permit(:edit)    }  # write the test in another way
+    it { should_not permit(:destroy) }
+  end
+
+
   context "for an admin" do
     let(:user)    { User.create!(email: "test@example.com", password: "test", confirmed: true, roles: ["admin"]) }
     let(:project) { Project.create!(name: "Project 1") }
@@ -40,5 +56,19 @@ describe TaskPolicy do
     it { should permit(:update)  }
     it { should permit(:edit)    }
     it { should permit(:destroy) }
+  end
+
+  context "for an admin on tasks of another user" do
+    let(:user)    { User.create!(email: "test@example.com", password: "test", confirmed: true, roles: ["admin"]) }
+    let(:dev)     { User.create!(email: "dev@example.com", password: "test", confirmed: true, roles: ["developer"]) }
+    let(:project) { Project.create!(name: "Project 1") }
+    let(:task)    { dev.tasks.create(name: "Task 2", status: "todo", project: project) }
+
+    it { should     permit(:show)    }
+    it { should_not permit(:create)  }  # fix the policy
+    it { should_not permit(:new)     }  # fix the policy
+    it { should     permit(:update)  }
+    it { should     permit(:edit)    }
+    it { should     permit(:destroy) }
   end
 end
